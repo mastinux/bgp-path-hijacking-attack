16c647283371056f33facdf12dadb705c9f300d1
mastinux <andrea.pantaleo.93@gmail.com>
2018-05-27 07:45:13 -0700

    modified .gitignore

--- bgp.py
+++ bgp.py
@@ -51,7 +51,6 @@ class Router(Switch):
         pass
 
     def stop(self):
-        # Delete all of our interfaces.
         self.deleteIntfs()
 
     def log(self, s, col="magenta"):
@@ -66,53 +65,38 @@ class SimpleTopo(Topo):
     def __init__(self):
         # Add default members to class.
         super(SimpleTopo, self ).__init__()
-        num_hosts_per_as = 1
+        num_hosts_per_as = 3
         num_ases = 3
         num_hosts = num_hosts_per_as * num_ases
-
         # The topology has one router per AS
 	routers = []
-
-        # adding routers
         for i in xrange(num_ases):
             router = self.addSwitch('R%d' % (i+1))
 	    routers.append(router)
-
         hosts = []
-
-        # adding hosts
         for i in xrange(num_ases):
             router = 'R%d' % (i+1)
-
             for j in xrange(num_hosts_per_as):
                 hostname = 'h%d-%d' % (i+1, j+1)
                 host = self.addNode(hostname)
                 hosts.append(host)
                 self.addLink(router, host)
 
-        # adding links (R1-R2) (R2-R3)
         for i in xrange(num_ases-1):
             self.addLink('R%d' % (i+1), 'R%d' % (i+2))
 
-        # rogue AS
         routers.append(self.addSwitch('R4'))
-
-        # adding hosts to rogue AS
         for j in xrange(num_hosts_per_as):
             hostname = 'h%d-%d' % (4, j+1)
             host = self.addNode(hostname)
             hosts.append(host)
             self.addLink('R4', hostname)
-
         # This MUST be added at the end
-        # adding link (R1-R4)
         self.addLink('R1', 'R4')
         return
 
 
 def getIP(hostname):
-    # retrieve host address from its name
-
     AS, idx = hostname.replace('h', '').split('-')
     AS = int(AS)
     if AS == 4:
@@ -122,8 +106,6 @@ def getIP(hostname):
 
 
 def getGateway(hostname):
-    # retrieve gateway address from host name
-
     AS, idx = hostname.replace('h', '').split('-')
     AS = int(AS)
     # This condition gives AS4 the same IP range as AS3 so it can be an
@@ -140,41 +122,32 @@ def startWebserver(net, hostname, text="Default web server"):
 
 
 def main():
-    # clearing logs
-    os.system("rm -f /tmp/R*.log /tmp/R*.pid logs/*")
-    # launching mininet
+    os.system("rm -f /tmp/R*.log /tmp/R*.pid logs/*stdout")
     os.system("mn -c >/dev/null 2>&1")
-    # killing zebra and bgpd
     os.system("killall -9 zebra bgpd > /dev/null 2>&1")
-    # killing the webserver
     os.system('pgrep -f webserver.py | xargs kill -9')
 
     net = Mininet(topo=SimpleTopo(), switch=Router)
     net.start()
     for router in net.switches:
-        # enabling IPv4 forwarding on router
         router.cmd("sysctl -w net.ipv4.ip_forward=1")
         router.waitOutput()
 
-    log("Waiting %d seconds for sysctl changes to take effect..." % args.sleep)
+    log("Waiting %d seconds for sysctl changes to take effect..."
+        % args.sleep)
     sleep(args.sleep)
 
     for router in net.switches:
         if router.name == ROGUE_AS_NAME and not FLAGS_rogue_as:
             continue
-
-        # configuring routing entries with zebra
         router.cmd("/usr/lib/quagga/zebra -f conf/zebra-%s.conf -d -i /tmp/zebra-%s.pid > logs/%s-zebra-stdout 2>&1" % (router.name, router.name, router.name))
         router.waitOutput()
-        # configuring bgpd (routing deamon) with quagga
         router.cmd("/usr/lib/quagga/bgpd -f conf/bgpd-%s.conf -d -i /tmp/bgp-%s.pid > logs/%s-bgpd-stdout 2>&1" % (router.name, router.name, router.name), shell=True)
         router.waitOutput()
         log("Starting zebra and bgpd on %s" % router.name)
 
     for host in net.hosts:
-        # configuring host IP address
         host.cmd("ifconfig %s-eth0 %s" % (host.name, getIP(host.name)))
-        # configuring default gateway
         host.cmd("route add default gw %s" % (getGateway(host.name)))
 
     log("Starting web servers", 'yellow')
